f2aa4cb0477e4c18d27f69547baac7b1081c02e7
Geraldine Bengsch <geraldine.bengsch@gmail.com>
2026-07-24 15:55:37 -0400

    change port

--- app.py
+++ app.py
@@ -221,4 +221,6 @@ def load_annotations(course, week, page):
 
 
 if __name__ == '__main__':
-    app.run()
+    # Use the PORT environment variable provided by Cloud Run
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
